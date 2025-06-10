ab2a9c6442d60380ec3f30824d8ab0f9e773bb07
Per1ova <azzabimourad2@gmail.com>
2025-06-10 13:16:20 +0200

    Initialize SharedPreferencesRepository class

---
+++ lib/data/shared_preferences_repository.dart
@@ -0,0 +1,34 @@
+import 'package:simple_beautiful_checklist_exercise/data/database_repository.dart';
+
+class SharedPreferencesRepository implements DatabaseRepository {
+  @override
+  Future<void> addItem(String item) {
+    // TODO: implement addItem
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> deleteItem(int index) {
+    // TODO: implement deleteItem
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> editItem(int index, String newItem) {
+    // TODO: implement editItem
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<int> getItemCount() {
+    // TODO: implement getItemCount
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<String>> getItems() {
+    // TODO: implement getItems
+    throw UnimplementedError();
+  }
+  // Implementation details
+}
