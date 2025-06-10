b7242c55bdd1aa97529e926e3d908eaf3de2d62d
Per1ova <azzabimourad2@gmail.com>
2025-06-10 13:25:10 +0200

    Implement methods in SharedPreferencesRepository for item management

--- lib/data/shared_preferences_repository.dart
+++ lib/data/shared_preferences_repository.dart
@@ -1,34 +1,45 @@
 import 'package:simple_beautiful_checklist_exercise/data/database_repository.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SharedPreferencesRepository implements DatabaseRepository {
+  final SharedPreferences _prefs;
+
+  SharedPreferencesRepository(this._prefs);
+
+  static const String _key = 'items';
+
   @override
-  Future<void> addItem(String item) {
-    // TODO: implement addItem
-    throw UnimplementedError();
+  Future<void> addItem(String item) async {
+    final items = await getItems();
+    items.add(item);
+    await _prefs.setStringList(_key, items);
   }
 
   @override
-  Future<void> deleteItem(int index) {
-    // TODO: implement deleteItem
-    throw UnimplementedError();
+  Future<void> deleteItem(int index) async {
+    final items = await getItems();
+    items.removeAt(index);
+    await _prefs.setStringList(_key, items);
   }
 
   @override
-  Future<void> editItem(int index, String newItem) {
-    // TODO: implement editItem
-    throw UnimplementedError();
+  Future<void> editItem(int index, String newItem) async {
+    final items = await getItems();
+    if (index >= 0 && index < items.length) {
+      items[index] = newItem;
+      await _prefs.setStringList(_key, items);
+    }
   }
 
   @override
-  Future<int> getItemCount() {
-    // TODO: implement getItemCount
-    throw UnimplementedError();
+  Future<int> getItemCount() async {
+    final items = await getItems();
+    return items.length;
   }
 
-  @override
-  Future<List<String>> getItems() {
-    // TODO: implement getItems
-    throw UnimplementedError();
+   @override
+  Future<List<String>> getItems() async {
+    return _prefs.getStringList(_key) ?? [];
   }
-  // Implementation details
+  
 }
